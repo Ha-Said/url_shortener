d02c6de9d16c4bee5b30b792e238898adafa9d59
Hadj Abdallah Said <saidhajabdallahs@gmail.com>
2026-04-29 17:36:44 +0100

    fix: remove process.exit, clean vercel config

--- url_shortener_backend/db.js
+++ url_shortener_backend/db.js
@@ -6,7 +6,7 @@ async function connectDB() {
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
-    process.exit(1);
+    throw err;
   }
 }
 
